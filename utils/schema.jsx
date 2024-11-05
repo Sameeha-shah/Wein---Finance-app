09fa85659ca11564f4214629d15de8dbaf1d494d
Sameeha Shah <165191675+Sameeha-shah@users.noreply.github.com>
2024-11-05 23:30:44 +0530

    Add files via upload

---
+++ utils/schema.jsx
@@ -0,0 +1,31 @@
+import {
+  integer,
+  numeric,
+  pgTable,
+  serial,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const Budgets = pgTable("budgets", {
+  id: serial("id").primaryKey(),
+  name: varchar("name").notNull(),
+  amount: numeric("amount").notNull().default(0),
+  icon: varchar("icon"),
+  createdBy: varchar("createdBy"),
+});
+
+export const Incomes = pgTable("incomes", {
+  id: serial("id").primaryKey(),
+  name: varchar("name").notNull(),
+  amount: numeric("amount").notNull().default(0),
+  icon: varchar("icon"),
+  createdBy: varchar("createdBy"),
+});
+
+export const Expenses = pgTable("expenses", {
+  id: serial("id").primaryKey(),
+  name: varchar("name").notNull(),
+  amount: numeric("amount").notNull().default(0),
+  budgetId: integer("budgetId").references(() => Budgets.id),
+  createdAt: varchar("createdAt").notNull(),
+});
